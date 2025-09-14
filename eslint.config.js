ce97350271b200708b6ef33d05d5a625ef87eeec
wyMinLwin <waiyanminlwin421@gmail.com>
2025-09-14 14:23:05 +0700

    chores: Set up code formatter

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,18 @@ import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { ignores: ["dist/*", "node_modules/*"]},
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		languageOptions: { globals: globals.browser }
+	},
+	tseslint.configs.recommended,
+	{
+		files: ["**/*.json"],
+		plugins: { json },
+		language: "json/json",
+		extends: ["json/recommended"]
+	},
+	{ ignores: ["dist/*", "node_modules/*"] }
 ]);
